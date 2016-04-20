9236b5f6fdb46b9e1f7a929e3fcd2dc451b09cca
Alexandrecorre <alexandre.corre@isep.fr>
2016-04-20 14:07:58 +0200

    add mvc page Sport

---
+++ Site/controleur/Sports.php
@@ -0,0 +1,11 @@
+<?php
+include_once('../modele/get_sports.php');
+$sports = get_sports(0,50);
+
+foreach($sports as $cle => $sport)
+{
+	$sports[$cle]['sport'] = htmlspecialchars($sport['sport']);
+}
+
+
+include_once('../vue/Sports.php');
---
+++ Site/vue/Sports.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Les Sports</title>
+		<link href="../vue/Sports.css" rel="stylesheet" type="text/css" media="screen" />
+		<meta http-equiv="content-type" content="text/html" charset="UTF-8" />
+	</head>
+	<body>
+		<!--
+		<?php
+		//include"../Header/Header.php";
+		?>
+	-->
+	
+	    <div class="HOMETOP" style="height: 100vh;">
+	    	<div class="menu">
+		       	<div id="left">
+		       		<a href="../Accueil/Accueil.php"><img src="../Images/Logos/Sporciety.png" style="	height: 4em;" /></a>
+			    </div>
+			    <ul id="right">
+		       	   	<li><a href="../Sports/Sports.php">LES SPORTS</a></li>
+			        <li><a href="../Forum/Forum.php">FORUM</a></li>
+			        <li><a href="../Contact/Contact.php">CONTACT</a></li>
+			        <li><a href="../Connexion/Connexion.php">CONNEXION</a></li>
+			        <li><a href="../Inscription/Inscription.php">INSCRIPTION</a></li>
+			        <li><a href="../Aide/Aide.php">AIDE</a></li>
+	       		</ul>
+	    	</div>
+	   
+			<div class="Sports">
+			    <table>
+			    	<tbody>
+						<?php
+						$a=0;
+						foreach($sports as $sport)
+						{	
+						?>
+						<td><a href="#"><?php echo mb_strtoupper($sport['sport'])?></a></td>
+						<?php
+							$a++;
+							if($a%3==0)
+							{
+								?>
+								</tr>
+								<tr>
+								<?php
+							}
+						}
+		    			?>
+		 				</tr>
+					</tbody>
+				</table>
+			</div>
+			<script type="text/javascript">
+ 			function AfficherMasquer()
+			{
+				divInfo = document.getElementById('divacacher');
+				if (divInfo.style.display == 'none')
+				    divInfo.style.display = 'block';
+				else
+				divInfo.style.display = 'none';
+			}
+			</script>
+			<div id="Plus"  onClick="AfficherMasquer(); scrollTo(0,400)">
+				<a>Plus…</a>
+			</div>
+			<div id="divacacher" style="display:none;">
+				<a>AUTRES SPORTS :</a>
+			</div>
+		</div> 
+	</body>
+</html>
