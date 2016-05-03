a78327d3690a2f2a2fd2868fadec31c4cdf4c361
Alexandrecorre <alexandre.corre@isep.fr>
2016-05-03 10:55:45 +0200

    maj

--- Site/modele/get_membres.php
+++ Site/modele/get_membres.php
@@ -1,5 +1,5 @@
 <?php
-function get_membres($AdresseMail){
+function get_membres(){
 	$bdd = new PDO('mysql:host=localhost;dbname=test_site_internet;charset=utf8', 'root', 'root');
 	$reponse = $bdd->query('SELECT * FROM membre');
 	$membres = $reponse->fetchAll();
