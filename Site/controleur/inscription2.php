199ca9a1a008f6c0888ec7c42cc065a8f364b612
Alexandrecorre <alexandre.corre@isep.fr>
2016-05-09 11:43:55 +0200

    finition inscription

--- Site/controleur/inscription2.php
+++ Site/controleur/inscription2.php
@@ -4,36 +4,49 @@ if(isset($_POST['Genre']) and isset($_POST['Prenom']) and isset($_POST['Nom']) a
 	if(filter_var($_POST['Adresse_Mail'],FILTER_VALIDATE_EMAIL))
 	{
 		$_POST['Mot_de_Passe']=htmlspecialchars($_POST['Mot_de_Passe']);
-		if(strlen($_POST['Mot_de_Passe'])>6)
+		if(strlen($_POST['Mot_de_Passe'])>=6)
 		{
 			include'../modele/inscription_membre.php';	
 			$_POST['Prenom']=htmlspecialchars($_POST['Prenom']);
 			$_POST['Nom']=htmlspecialchars($_POST['Nom']);
-			$_POST['Date_de_Naissance']=htmlspecialchars($_POST['Date_de_Naissance']);
-			$_POST['Numéro_de_Téléphone']=(int)($_POST['Numéro_de_Téléphone']);
-			$_POST['Adresse']=htmlspecialchars($_POST['Adresse']);
+			if(isset($_POST['Date_de_Naissance']))
+			{
+				$Naissance=htmlspecialchars($_POST['Date_de_Naissance']);
+			}
+			else{$Naissance='';}
+			if(isset($_POST['Numéro_de_Téléphone']))
+			{
+				$telephone=(int)($_POST['Numéro_de_Téléphone']);
+			}
+			else{$telephone='';}
+			if(isset($_POST['Adresse']))
+			{
+				$adresse=htmlspecialchars($_POST['Adresse']);
+			}
+			else{$adresse='';}
 			$_POST['Code_Postal']=htmlspecialchars($_POST['Code_Postal']);
 			
-			inscription_membre($_POST['name'],$_POST['Nom'],$_POST['Prenom'],$_POST['Date_de_Naissance'],$_POST['Code_Postal'],$_POST['Adresse'],$_POST['Adresse_Mail'],$_POST['Numéro_de_Téléphone'],$_POST['Mot_de_Passe']);	
+			inscription_membre($_POST['Genre'],$_POST['Nom'],$_POST['Prenom'],$Naissance,$_POST['Code_Postal'],$adresse,$_POST['Adresse_Mail'],$telephone,$_POST['Mot_de_Passe']);	
 			include'Accueil.php';
 		}
 		else
 		{
 			$erreur='Mot de passe invalide 6 caractères minimum';
-			return $erreur;
-		}	include'Inscription.php';
+			//return $erreur;
+			//include'Inscription.php';
+		}	
 	}
 	else
 	{
 		$erreur='Adresse mail invalide';
-		return $erreur;
-		include'Inscription.php';
+		//return $erreur;
+		//include'Inscription.php';
 	}
 }
 else
 {
 	$erreur='informations incomplètes';
-	include'Inscription.php';
+	//include'Inscription.php';
 }
 
 echo $erreur;
