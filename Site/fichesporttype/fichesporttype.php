3547760867a9d0c315bb48084088e5821c7fc15e
JLHub1 <Jean@MacBook-Pro-de-Jean.local>
2016-05-10 12:00:52 +0200

    MàJ

--- Site/fichesporttype/fichesporttype.php
+++ Site/fichesporttype/fichesporttype.php
@@ -12,13 +12,25 @@
 		       		<a href="../controleur/Accueil.php"><img src="../Images/Logos/Sporciety.png" style="height: 3.8em;" /></a>
 			    </div>
 			    <ul id="right">
-		       	   	<li><a href="../controleur/Sports.php">LES SPORTS</a></li>
-		           	<li><a href="../controleur/Forum.php">FORUM</a></li>
-		           	<li><a href="../controleur/Contact.php">CONTACT</a></li>
-		           	<li><a href="../controleur/Connexion.php">CONNEXION</a></li>
-		           	<li><a href="../controleur/Inscription.php">INSCRIPTION</a></li>
-	         		<li><a href="../controleur/Aide.php">AIDE</a></li>
-	       		</ul>
+			          <li><a href="../controleur/Sports.php">LES SPORTS</a></li>
+			          <li><a href="../controleur/Forum.php">FORUM</a></li>
+			          <li><a href="../controleur/Contact.php">CONTACT</a></li>
+			          <?php if(isset($_SESSION['mail'])){
+			          	?>
+			          	<li><a href="../controleur/session_destroy.php">DECONNEXION</a></li>
+			          	<li><a href="../controleur/Page_Personnelle.php">PROFIL</a></li>
+			          	<?php
+			          }
+			          else
+			          {
+			          	?>
+			          <li><a href="../controleur/Connexion.php">CONNEXION</a></li>
+			          <li><a href="../controleur/Inscription.php">INSCRIPTION</a></li>
+			          <?php
+			          }
+			          ?>
+			          <li><a href="../controleur/Aide.php">AIDE</a></li>
+			      </ul>
 	    	</div>
 	    	<div class="center">
 	    		<div class="titre">
