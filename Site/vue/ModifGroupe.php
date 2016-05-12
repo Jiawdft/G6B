a0a5ab0dee53d05ca1d247a6ecd9b34f56fa62cd
Logan Lemoine <logan.lemoine12@gmail.com>
2016-05-12 12:13:26 +0200

    a

--- Site/vue/ModifGroupe.php
+++ Site/vue/ModifGroupe.php
@@ -46,9 +46,15 @@
 		    				<tr>
 		    					<ul>
 		    						<li>
-		    							<?php
-		    							echo ;
-		    							?>
+		    							<?php 
+		    							
+										include_once'../modele/get_membres_groupes.php';
+										$go=get_membres_groupes('','');
+										foreach ($go as $test) {
+											echo $test['groupe'];
+										}
+
+										 ?>
 		    						</li>
 		    					</ul>
 		    				</tr>
