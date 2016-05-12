e9630fb48c6dc37f682adbcfadd18f488b210baa
Logan Lemoine <logan.lemoine12@gmail.com>
2016-05-12 12:40:56 +0200

    test header

---
+++ Test/Connexion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Connexion</title>
+		<link href="../Test/Connexion.css" rel="stylesheet" type="text/css" media="screen" />
+		<meta http-equiv="content-type" content="text/html" charset="UTF-8" />
+	</head>
+	<body>
+
+		<?php include("../Test/HOMETOP.php"); ?>
+
+	    <div class="Connexion">
+		  <h1 style="text-align: center;">CONNECTEZ-VOUS</h1>
+		  <form>
+		    <input type="email" name="Adresse Mail" placeholder="Adresse Mail" />
+		    <input type="password" name="Mot de Passe" placeholder="Mot de Passe" />
+		    <input type="submit" value="CONNEXION" />
+		  </form>
+		  <a id="inscription" href="#">Pas encore inscrit ? Inscris toi !</a>
+		  <div id="social" style="text-align: center; margin-top: 10%">
+			<a href="https://www.facebook.com/workoutsociety/?ref=aymt_homepage_panel"><img src="../Images/Logos/Fb.png"></a>
+	    	<a href="https://twitter.com/?lang=fr"><img src="../Images/Logos/Tw.png"></a>
+			<a href="https://www.instagram.com"><img src="../Images/Logos/Insta.png"></a>
+	    	<a href="https://www.pinterest.com"><img src="../Images/Logos/Pint.png"></a>
+		</div>
+	</body>
+</html>
