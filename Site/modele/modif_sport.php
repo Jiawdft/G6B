37e4dfe00304ab863682da6e95513659b5d9206a
Alexandrecorre <alexandre.corre@isep.fr>
2016-05-16 23:55:37 +0200

    maj

---
+++ Site/modele/modif_sport.php
@@ -0,0 +1,20 @@
+<?php
+function modif_sport($Nom,$Info){
+	$Nom=htmlspecialchars($Nom);
+	$Info=htmlspecialchars($Info);	
+	$bdd = new PDO('mysql:host=localhost;dbname=test_site_internet;charset=utf8', 'root', 'root');
+	if(!isset($Info)or $Info=='')
+	{
+
+	}
+	 else
+	{
+	$req = $bdd->prepare('UPDATE sport SET information = :information WHERE sport =:sport');
+		$req->execute(array(
+			'sport' => $Nom,
+			'information' => $Info
+
+	));
+	}
+}
+?>
