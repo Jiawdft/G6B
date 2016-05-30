07564e4fa14a52f3a63829c4f00b6d90f8d98569
Alexandrecorre <alexandre.corre@isep.fr>
2016-05-30 09:36:54 +0200

    maj page club

---
+++ Site/controleur/Club.php
@@ -0,0 +1,13 @@
+<?php
+if(!isset($_SESSION['mail']))
+{
+session_start();
+}
+if(!isset($_GET['club'])){
+	include_once('../controleur/Accueil.php');
+}else{
+	
+
+include_once('../vue/Club.php');
+}
+?>
---
+++ Site/vue/Club.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $_GET['club'] ?></title>
+		<link href="../vue/Club.css" rel="stylesheet" type="text/css" media="screen" />
+		<meta http-equiv="content-type" content="text/html" charset="UTF-8" />
+	</head>
+	<body>
+		
+		<?php include("../Header/HOMETOP.php"); ?>
+		
+		<div class="user">
+			<img src="../Images/Logos/User.png" style="height: 7em;">
+			<div id="uinfo">
+				<table>
+					<tr>
+						<th>
+						<?php
+						include'../modele/get_club.php';
+						$club=get_club($_GET['club'],'');
+							echo $club['nom'].'</br>'.$club['code_postal'].'</br>'.$club['description'];
+						
+						?>
+						</th>
+					</tr>
+					
+			<div class="more">
+				<div id="p">
+					<h3>Les Groupes :</h3>
+					<table>
+						<tr>
+							<th>
+								<ul><?php
+								include'../modele/get_groupe_club.php';
+								$groupes=get_groupe_club('',$_GET['club']);
+										foreach ($groupes as $groupe) {
+											?><li><a href="../controleur/Page_Personnelle_mon_groupe.php?groupe=<?php echo $groupe['groupe']?>" ><?php echo $groupe['groupe']?></li><?php
+										}
+																		
+									?>								
+								</ul>
+							</th>
+						</tr>
+					</table>
+				</div>
+			</div>	
+	</body>
+</html>
