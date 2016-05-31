b6b311cef8861ce082da6a5960e9c576340ff423
Logan Lemoine <logan.lemoine12@gmail.com>
2016-05-31 15:15:02 +0200

    divers

--- Site/controleur/new_event.php
+++ Site/controleur/new_event.php
@@ -3,8 +3,11 @@ if(!isset($_SESSION['mail']))
 {
 	session_start();
 }
+if(isset($_POST['resultat_nom_event'])){
 
-include_once'../modele/new_event.php';
 new_event($_POST['resultat_nom_event'],$_POST['resultat_description'],$_POST['resultat_date'],$_POST['resultat_adresse'],$_GET['groupe']);
-
+include_once'../controleur/Groupe.php';	
+}else{
+include_once'../vue/new_event.php';
+}
 ?>
