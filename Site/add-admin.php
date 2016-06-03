fd39c24e3e89b10180d2e8370a1bc2fce2ff8d43
JLHub1 <Jean@MacBook-Pro-de-Jean.local>
2016-06-03 10:20:15 +0200

    push

--- Site/add-admin.php
+++ Site/add-admin.php
@@ -8,15 +8,16 @@ $Mdp='coucou';
 	$AdresseMail=htmlspecialchars($AdresseMail);
 	$Mdp=htmlspecialchars($Mdp);
 	*/
+	function inscription_membre($test,$moi){
 	$bdd = new PDO('mysql:host=localhost;dbname=test_site_internet;charset=utf8', 'root', 'root');
-	$req = $bdd->query('INSERT INTO admin(adresse_mail,passe) 
+	$req = $bdd->prepare('INSERT INTO admin(adresse_mail,passe) 
 		VALUES(:adresse_mail,:passe');
 	$req->execute(array(
-	'adresse_mail' => $AdresseMail,
-	'passe'=> $Mdp
-	));
+	'adresse_mail' => $test,
+	'passe'=> $moi
+	));}
 
-
+inscription_membre($AdresseMail,$Mdp);
 echo 'ok ca marche';
 
 
