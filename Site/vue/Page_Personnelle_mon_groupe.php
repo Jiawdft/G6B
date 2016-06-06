17919b884e0490c30625ddd56e12ffab2001708d
JLHub1 <Jean@MacBook-Pro-de-Jean.local>
2016-06-06 12:02:22 +0200

    MàJ

--- Site/vue/Page_Personnelle_mon_groupe.php
+++ Site/vue/Page_Personnelle_mon_groupe.php
@@ -65,7 +65,7 @@
 				</div>
 				<div id="cal">
 					<h3>Le Calendrier :</h3>
-					<a href="../controleur/new_event.php?groupe=<?php echo $_GET['groupe'] ?>">Créer un événement</a>
+					<p><a href="../controleur/new_event.php?groupe=<?php echo $_GET['groupe'] ?>">Créer un événement</a></p>
 					<table>
 						<tr>
 							<th>
