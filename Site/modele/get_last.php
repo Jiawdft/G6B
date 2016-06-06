150d99410b9ea8d5259dfa627fc0f70c9b177a4c
Alexandrecorre <alexandre.corre@isep.fr>
2016-06-06 20:08:19 +0200

    get last

--- Site/modele/get_last.php
+++ Site/modele/get_last.php
@@ -1,7 +1,7 @@
 <?php
 function get_last()
 {
-	include_once'connexion_sql.php';
+	include'connexion_sql.php';
 
 	$reponse = $bdd->query('SELECT * FROM groupe ORDER BY date_creation DESC LIMIT 0, 5');
 	$groupes = $reponse->fetchAll();
