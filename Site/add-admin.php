8e206457afa0b25c21b199885656e504112f7702
Alexandrecorre <alexandre.corre@isep.fr>
2016-06-06 20:33:55 +0200

    maj add

--- Site/add-admin.php
+++ Site/add-admin.php
@@ -7,16 +7,16 @@ $Mdp='coucou';
 
 	$AdresseMail=htmlspecialchars($AdresseMail);
 	$Mdp=htmlspecialchars($Mdp);
-	function inscription_membre(){
+	function inscription_membre($AdresseMail,$Mdp){
 	include_once'/modele/connexion_sql.php';
 	$req = $bdd->prepare('INSERT INTO admin(adresse_mail,passe) 
 		VALUES(:adresse_mail,:passe)');
 	$req->execute(array(
-	'adresse_mail' => 'o@gmail.com',
-	'passe'=> password_maker('coucou')
+	'adresse_mail' => $AdresseMail,
+	'passe'=> password_maker($Mdp)
 	));}
 
-inscription_membre();
+inscription_membre($AdresseMail,$Mdp);
 echo 'ok ca marche';
 
 ?>
